793626774431482ef3f271d603ab503dbda70fbe
Liam Sutton <liamsutton1997@outlook.com>
2021-04-27 13:14:41 +0100

    Added switch controlled doors

---
+++ Assets/_Scripts/DoorController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    public GameObject LeftDoor;
+    public GameObject RightDoor;
+    public Transform LeftDoorEndPoint;
+    public Transform RightDoorEndPoint;
+    private bool shouldHide = false;
+    public float doorMoveSpeed;
+
+    private void Update()
+    {
+        if (shouldHide)
+        {
+            OpenDoor();
+        }
+    }
+
+    private void OpenDoor()
+    {
+        float step = doorMoveSpeed * Time.deltaTime;
+
+        LeftDoor.transform.position = Vector3.MoveTowards(LeftDoor.transform.position, LeftDoorEndPoint.transform.position, step);
+        RightDoor.transform.position = Vector3.MoveTowards(RightDoor.transform.position, RightDoorEndPoint.transform.position, step);
+    }
+
+    public void TriggerHide()
+    {
+        shouldHide = true;
+    }
+}
