088ad8e1f2f43d2bf3cb7f9ee74435faee8815e5
Liam Sutton <liamsutton1997@outlook.com>
2021-04-28 10:51:21 +0100

    Hooked switch upto door

---
+++ Assets/_Scripts/GazeInteractionController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GazeInteractionController : MonoBehaviour
+{
+    public Transform camera;
+    public float baseFuseTimerLength = 2f;
+    public float fuseTimer = 2f;
+    void Update()
+    {
+        Ray ray;
+        RaycastHit hit;
+        GameObject hitObject;
+
+        ray = new Ray(camera.position, camera.rotation * Vector3.forward);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            hitObject = hit.collider.gameObject;
+
+            if (hitObject.CompareTag("Switch"))
+            {
+                if (fuseTimer <= 0)
+                {
+                    // Click the button and trigger interaction
+                    hitObject.SendMessage("Activate");
+                    fuseTimer = baseFuseTimerLength;
+                }
+                else
+                {
+                    fuseTimer -= Time.deltaTime;
+                }
+            }
+            else
+            {
+                fuseTimer = baseFuseTimerLength;
+            }
+        }
+    }
+}
---
+++ Assets/_Scripts/SwitchInteractionController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchInteractionController : MonoBehaviour
+{
+    public GameObject door;
+    private DoorController doorController;
+
+    public void Awake()
+    {
+        doorController = door.GetComponent<DoorController>();
+    }
+
+    public void Activate()
+    {
+        doorController.TriggerHide();
+    }
+}
