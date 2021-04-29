36c1d39ce651cfbf0aeda3a3a6d4053b6bf7832c
Liam Sutton <liamsutton1997@outlook.com>
2021-04-29 13:19:44 +0100

    Button now responds to dropped cube

---
+++ Assets/_Scripts/ButtonController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{
+    public AudioClip buttonHitSoundEffect;
+    private bool isActive = false;
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Cube"))
+        {
+            Activate();
+        }
+    }
+
+    public void Activate()
+    {
+        isActive = true;
+        AudioSource.PlayClipAtPoint(buttonHitSoundEffect, transform.transform.position);
+    }
+}
