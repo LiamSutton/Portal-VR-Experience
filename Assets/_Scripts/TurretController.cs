898a3250ca87af2888c6a7fd7038785dc4c7a6df
Liam Sutton <liamsutton1997@outlook.com>
2021-05-03 13:18:34 +0100

    Turret Controller can now trigger animations from code

--- Assets/_Scripts/TurretController.cs
+++ Assets/_Scripts/TurretController.cs
@@ -6,10 +6,37 @@ public class TurretController : MonoBehaviour
 {
     public AudioClip[] voiceLines;
     public Animator animator;
-
+    public float timer = 3.0f;
+    public bool played = false;
+    public void Update()
+    {
+        if (timer <= 0 && !played)
+        {
+            StartCoroutine("Deploy");
+        }
+        else
+        {
+            timer -= Time.deltaTime;
+        }
+    }
     public void PlayVoiceLine()
     {
         int idx = Random.Range(0, voiceLines.Length);
         AudioSource.PlayClipAtPoint(voiceLines[idx], transform.position);
+    }
+
+    public void Animate()
+    {
+
+    }
+
+    public IEnumerator Deploy()
+    {
+        played = true;
+        PlayVoiceLine();
+        animator.Play("Base Layer.Turret_Deploy");
+        yield return new WaitForSeconds(2f);
+        played = false;
+        timer = 3.0f;
     }
 }
