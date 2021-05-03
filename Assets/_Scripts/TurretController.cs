604399fa6ef5306a05396426c2863500b093a1a3
Liam Sutton <liamsutton1997@outlook.com>
2021-05-03 13:02:22 +0100

    Added voice lines for turret

---
+++ Assets/_Scripts/TurretController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretController : MonoBehaviour
+{
+    public AudioClip[] voiceLines;
+    public Animator animator;
+
+    public void PlayVoiceLine()
+    {
+        int idx = Random.Range(0, voiceLines.Length);
+        AudioSource.PlayClipAtPoint(voiceLines[idx], transform.position);
+    }
+}
