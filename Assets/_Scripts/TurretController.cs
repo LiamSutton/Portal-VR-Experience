145c2bcf9254d00481daf2d9c39d714885350874
Liam Sutton <liamsutton1997@outlook.com>
2021-05-05 10:46:29 +0100

    Began adding functionality to conveyer belt

--- Assets/_Scripts/TurretController.cs
+++ Assets/_Scripts/TurretController.cs
@@ -5,26 +5,65 @@ using UnityEngine;
 public class TurretController : MonoBehaviour
 {
     public AudioClip[] voiceLines;
+    public GameObject[] positions;
     public Animator animator;
-    public float timer = 3.0f;
-    public bool played = false;
 
-    // removed the playing logic for now as I will add triggers later on
+    private int currentTargetIndex = 0;
+    private GameObject currentTarget;
+    private float speed = 2.5f;
 
-    private void Awake() => StartCoroutine("Deploy");
+    private void Awake()
+    {
+        currentTarget = positions[0];
+    }
+
+    private void Update()
+    {
+        if (transform.position == currentTarget.transform.position)
+        {
+            PickNewTarget();
+        }
+        MoveToTarget();
+    }
     public void PlayVoiceLine()
     {
         int idx = Random.Range(0, voiceLines.Length);
         AudioSource.PlayClipAtPoint(voiceLines[idx], transform.position);
     }
 
-    public IEnumerator Deploy()
+    public void Deploy()
     {
-        played = true;
         PlayVoiceLine();
         animator.Play("Base Layer.Turret_Deploy");
-        yield return new WaitForSeconds(2f);
-        played = false;
-        timer = 3.0f;
+    }
+
+    public void MoveToTarget()
+    {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, currentTarget.transform.position, step);
+    }
+
+    public void PickNewTarget()
+    {
+        currentTargetIndex++;
+
+        if (currentTargetIndex >= positions.Length)
+        {
+            currentTargetIndex = 0;
+            currentTarget = positions[currentTargetIndex];
+            transform.position = positions[0].transform.position;
+        }
+        else
+        {
+            currentTarget = positions[currentTargetIndex];
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("TurretDeploy"))
+        {
+            Deploy();
+        }
     }
 }
